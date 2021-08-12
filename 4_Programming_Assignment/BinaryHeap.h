95101f764238e908a1161255827b67602273f358
claytonkristiansen <clayton.kristiansen@hotmail.com>
2021-08-12 00:21:10 -0700

    getting stuff

--- 4_Programming_Assignment/BinaryHeap.h
+++ 4_Programming_Assignment/BinaryHeap.h
@@ -12,17 +12,13 @@
 template <typename T>
 class BinaryHeap {
     public:
-
+        //Initialize binary heap as empty
         BinaryHeap()
         {
             elements = std::vector<T>();
             size = 0;
         }
-
-        //Implement the below 4 methods. Please note these methods are not from MPQ.h
-        //BinaryHeap class does not inherit MPQ class 
-        //Implement BinaryHeap insert operation
-        //Also implement upheap and downheap operations (functions provided below) and call them from wherever necessary
+        //Insert new element
         void insert(const T& data)
         {
             elements.push_back(data);
@@ -58,15 +54,13 @@ class BinaryHeap {
             return min;
         }
     private:
-        //
+        //Vector of elements 
         std::vector<T> elements;
         int size = 0;
         //Implement down heap operation
         void down_heap(int i);
         //Implement up heap operation
         void up_heap(int i);
-
-        //Pre-implemented helper functions
         //swaps two elements at positions a and b
         void swap(int a, int b) { T c(elements[a]); elements[a] = elements[b]; elements[b] = c;}
         //Binary tree (Complete Binary tree) can be represented in vector form and hence, stored in a vector. 
--- 4_Programming_Assignment/BinaryHeapMPQ.h
+++ 4_Programming_Assignment/BinaryHeapMPQ.h
@@ -11,11 +11,6 @@
 template <typename T>
 class BinaryHeapMPQ : MPQ<T>
 {
-    // Implement the four funtions (insert, is_empty, min, remove_min) from MPQ.h
-    // It is enough if the 4 methods in  BinaryHeapMPQ calls the corresponding implemented methods in BinaryHeap.h.
-    // To hold the elements use BinaryHeap (from BinaryHeap.h)
-    // For remove_min() and min() throw exception if the BinaryHeapMPQ is empty. Mimir already has a try/catch block so don't use try/catch block here.
-
 private:
     BinaryHeap<T> m_heap;
 
